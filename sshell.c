649db7b2e5caf55546ba2c40edc9e26d6607d305
Lucas Nguyen <lhvnguyen@ucdavis.edu>
2023-04-11 13:54:16 -0700

    Phase 0 & 1 done

--- sshell.c
+++ sshell.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
 
-int main(void)
-{
 	/* test comment */
-
+int main(void){
         char cmd[CMDLINE_MAX];
 
         while (1) {
                 char *nl;
-                int retval;
+//                int retval;
 
                 /* Print prompt */
-                printf("sshell$ ");
+                printf("sshell@ucd$ ");
                 fflush(stdout);
 
                 /* Get command line */
@@ -40,10 +39,28 @@ int main(void)
                 }
 
                 /* Regular command */
-                retval = system(cmd);
+                if(!fork()){
+			execlp("/bin/sh", "sh", "-c", cmd, (char*)NULL);
+			perror("execv");
+			exit(1);
+		}
+		else{
+			int status;
+			int exit_status;
+			waitpid(-1, &status, 0);
+			if(WIFEXITED(status)){
+				exit_status = WEXITSTATUS(status);
+				if(!exit_status){
+					fprintf(stderr, "+ completed \'%s\' [%d]\n", cmd, WEXITSTATUS(status));
+				}
+			}
+		}
+/*		retval = system(cmd);
                 fprintf(stdout, "Return status value for '%s': %d\n",
                         cmd, retval);
+			*/
         }
 
         return EXIT_SUCCESS;
+	
 }
