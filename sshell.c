f5174bdd48de8d34b29752f062b7a87d81f6fcfb
Youssef Qteishat <ysqteishat@ucdavis.edu>
2023-04-11 16:02:56 -0700

    Phase 2 - Arguments

--- sshell.c
+++ sshell.c
@@ -6,9 +6,27 @@
 
 #define CMDLINE_MAX 512
 
-	/* test comment */
+struct prog{
+        char* pn; // program name
+        char argArr[16][2]; // program argument array
+};
+/*
+// populates array with empty strings
+void newProg(struct prog *p, char* pp ){
+        strcpy(p->pn, pp);
+
+        for (int i=0; i< 16; i++){
+                strcpy(p->argArr[i], "");
+        }
+}
+*/
+
+
+
+
 int main(void){
         char cmd[CMDLINE_MAX];
+        char cmdCopy[CMDLINE_MAX];
 
         while (1) {
                 char *nl;
@@ -32,14 +50,45 @@ int main(void){
                 if (nl)
                         *nl = '\0';
 
+                strcpy(cmdCopy,cmd);
+                char* token = strtok(cmdCopy, " ");
+
+                struct prog program;
+                program.pn = token;
+                token = strtok(NULL, " ");
+                int count = 0;
+                while(token){
+                        if (token[0] == '-'){
+                                strcpy(program.argArr[count], token);
+                                count++;
+                        }
+
+                        token = strtok(NULL, " ");
+                }
+
+                char args[48];
+                for (int i = 0; i < count+1; i ++){
+                        if(i == 0){
+                                strcpy(args, program.argArr[i]);
+                        }
+                        else{
+                                strcat(args, program.argArr[i]);
+                        }
+                }
+                printf("The args are %s\n", args);
+
+
                 /* Builtin command */
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
+
+
                 /* Regular command */
                 if(!fork()){
+
 			execlp("/bin/sh", "sh", "-c", cmd, (char*)NULL);
 			perror("execv");
 			exit(1);
@@ -51,10 +100,12 @@ int main(void){
 			if(WIFEXITED(status)){
 				exit_status = WEXITSTATUS(status);
 				if(!exit_status){
-					fprintf(stderr, "+ completed \'%s\' [%d]\n", cmd, WEXITSTATUS(status));
+					fprintf(stderr, "+ completed \'%s\' [%d]\n", program.pn, WEXITSTATUS(status));
 				}
 			}
 		}
+
+
 /*		retval = system(cmd);
                 fprintf(stdout, "Return status value for '%s': %d\n",
                         cmd, retval);
